85580cd0cec7e6d74d5ca429ae9f65f29a62ffcb
ZhadanD <zhdv07@mail.ru>
2023-12-23 18:48:22 +0300

    Реализовал функционал дружбы между пользователями;

--- search_friend/script.js
+++ search_friend/script.js
@@ -1,3 +1,28 @@
+async function makeFriend(friendId) {
+    let url = 'http://127.0.0.1:8000/api/users/make_friend'
+
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            friendId: friendId
+        })
+    });
+
+    if(response.status === 401) document.location.href = '/auth/login'
+
+    let result = await response.json()
+
+    if(response.status === 422) alert(result.friendId[0])
+
+    if(response.status === 201) {
+        document.getElementById(friendId).innerHTML = '<button disabled class="btn btn-warning">Уже дружите</button>'
+    }
+}
+
 function getTable(result) {
     document.getElementById('title_table').innerText = `Всего пользователей: ${result.meta.total}`
 
@@ -8,7 +33,7 @@ function getTable(result) {
         <tr>
             <td width="150"><img width="150" src="/public/dist/img/man.png" alt="Аватар пользователя" class="img-circle img-fluid"></td>
             <td width="250">${result.data[i].name}</td>
-            <td width="100">${result.data[i].isFriend ? '<button disabled class="btn btn-warning">Уже дружите</button>' : '<button type="button" class="btn btn-success" onclick="makeFriend(${result.data[i].id})">Подружиться</button>'}</td>
+            <td width="100" id="${result.data[i].id}">${result.data[i].isFriend ? '<button disabled class="btn btn-warning">Уже дружите</button>' : `<button type="button" class="btn btn-success" onclick="makeFriend(${result.data[i].id})">Подружиться</button>`}</td>
         </tr>`
     }
 
